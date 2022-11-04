db60ce8a5003ef359c3ca0fe5d29d9c394f4ce6b
LiamBrem <liam.e.brem@gmail.com>
2022-11-04 17:49:41 -0400

    it prints the json info of the symbol

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ import requests
 
 API_KEY = "dQ_0vBbHkOiJyjlF4QQ8vK_Mq5Ss2eKp"
 
-API_URL = (
-    )
-print(API_URL)
-
 app = Flask(__name__)
 
 
 def fetchStockInfo(symbol):
-    API_URL = (f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey={API_KEY}")
-    print(API_URL)
+    API_URL = (f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2022-11-03/2022-11-04?adjusted=true&sort=asc&limit=120&apiKey={API_KEY}")
+    res = requests.get(API_URL)
+
+    print(str(res.json()))
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         symbol = request.form.get("symbol", "")
-
 
         fetchStockInfo(symbol)
         return "Your symbol is " + symbol
