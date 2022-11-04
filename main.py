25b6962f5cf3355da2da8609ddee7a33d98cf18b
LiamBrem <liam.e.brem@gmail.com>
2022-11-04 17:53:03 -0400

    displays/returns JSON in the html

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ def fetchStockInfo(symbol):
     API_URL = (f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2022-11-03/2022-11-04?adjusted=true&sort=asc&limit=120&apiKey={API_KEY}")
     res = requests.get(API_URL)
 
-    print(str(res.json()))
+    return str(res.json())
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         symbol = request.form.get("symbol", "")
 
-        fetchStockInfo(symbol)
-        return "Your symbol is " + symbol
+        return fetchStockInfo(symbol)
 
     return render_template("index.html")
 
