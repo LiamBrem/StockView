d6ef4140898df64fcf21cc20c0d5d9a5b1169836
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2022-11-04 14:11:53 +0000

    testing in main

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
+def multiplyByThree(number):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        return str(int(number) * 3)
+    except ValueError:
+        return "Not a number"
+
+
+@app.route("/")
 def index():
-   return "hello world"
+    num = request.args.get("number", "")
+    if num:
+        finalNumber = multiplyByThree(num)
+    else:
+        finalNumber = ""
+    return (
+        """<form action="" method="get">
+                Initial Number: <input type="text" name="number">
+                <input type="submit" value="Multiply by 3">
+            </form>"""
+        + "final Num: "
+        + finalNumber
+    )
+
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
