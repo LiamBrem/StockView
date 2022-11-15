c03bb34b969ec19ded6030915492d63c5fc3500e
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2022-11-15 19:15:13 +0000

    added some basic starter code to api functions

--- api_functions.py
+++ api_functions.py
@@ -1,4 +1,25 @@
-import yfinance as yf
+from datetime import date, timedelta
 
-msft = yf.Ticker("MSFT")
-print(msft.history(period="max"))
+import pandas as pd
+import yfinance as yf
+from matplotlib import pyplot as plt
+
+Start = date.today() - timedelta(365)
+Start.strftime('%Y-%m-%d')
+
+End = date.today() + timedelta(2)
+End.strftime('%Y-%m-%d')
+
+def closing_price(ticker):
+    Asset = pd.DataFrame(yf.download(ticker, start=Start,
+      end=End)['Adj Close'])     
+    return Asset
+
+TESLA = closing_price('TSLA')                  
+AMAZON = closing_price('AMZN')
+
+plt.plot(TESLA)
+plt.show()
+
+plt.plot(AMAZON)
+plt.show()
