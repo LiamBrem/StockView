308e96191a5ba4ea2435f94ed09b4648b76f640a
LiamBrem <liam.e.brem@gmail.com>
2022-11-15 20:34:19 -0500

    returns history of symbol

--- api_functions.py
+++ api_functions.py
@@ -7,8 +7,22 @@ from matplotlib import pyplot as plt
 import yfinance as yf
 
 def stockInfo(symbol):
+  #this initializes this shit or sum
   symbol = yf.Ticker(symbol)
 
-  return symbol.info
+  #gets all the information each minute of the entire previous day
+  #we should make the period and interval variables
+  symbolHistory = symbol.history(period="1d",interval="1m")
+
+  #Each item is just stored in a list within a list
+  #this takes the first value of all the Closed values
+  #could potentially iterate through it
+  #if you just do print(symbolHistory) then it will give you everything
+  print(symbolHistory["Close"][1])
+  
+
+  #bruh 
+  return symbol.history(period="1d", interval="1m")
 
   
+stockInfo("SPY")
