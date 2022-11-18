5e581fbc41394152b73915d90a9ccca9b3440a4f
LiamBrem <liam.e.brem@gmail.com>
2022-11-17 20:43:28 -0500

    NEEDS FIXING

--- main.py
+++ main.py
@@ -7,17 +7,14 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
+
+        #this isn't getting Stored
         symbol = request.form.get("symbol", "")
         print("\n" + symbol)
 
-        
-        inputPeriod = request.form.get("dateInput")
+        period = "1h"
+        interval = "1d"
 
-<<<<<<< HEAD
-        interval = checkInterval(inputPeriod)
-
-        return render_template("displayStock.html", info=fetchStockInfo(symbol, inputPeriod, interval))
-=======
         #Figure out how to put this into a seperate function
         if request.form.get('hour') == "1h":
             period = "1h"
@@ -37,11 +34,8 @@ def index():
         elif request.form.get("year") == "1y":
             period = "1y"
             interval = "1wk"
-            
-            
-            
+
         return render_template("displayStock.html", info=fetchStockInfo(symbol, period, interval))
->>>>>>> temp-branch
 
     return render_template("index.html")
 
@@ -49,32 +43,6 @@ def index():
 def fetchStockInfo(symbol, period, interval):
     return str(api_functions.stockInfo(symbol, period, interval))
 
-
-def checkInterval(inputPeriod):
-    # default period/interval
-    period = "1d"
-    interval = "1m"
-
-    if inputPeriod == "1h":
-        period = "1h"
-        interval = "1m"
-    elif inputPeriod == "1d":
-        period = "1d"
-        interval = "1m"
-    elif inputPeriod == "1wk":
-        period = "1wk"
-        interval = "1d"
-    elif inputPeriod == "1mo":
-        period = "1mo"
-        interval = "1d"
-    elif inputPeriod == "6mo":
-        period = "6mo"
-        interval = "1d"
-    elif inputPeriod == "1y":
-        period = "1y"
-        interval = "1wk"
-
-    return interval
 
 
 if __name__ == '__main__':
