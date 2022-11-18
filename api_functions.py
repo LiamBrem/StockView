d17e489098067a7be1725e2c6a59c894ef3028db
LiamBrem <liam.e.brem@gmail.com>
2022-11-17 19:43:20 -0500

    User selects interval with buttons

--- api_functions.py
+++ api_functions.py
@@ -3,28 +3,27 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-#source of stocks api
+# source of stocks api
 import yfinance as yf
 
-def stockInfo(symbol):
-  #this initializes this shit or sum
-  symbol = yf.Ticker(symbol)
 
-  #gets all the information each minute of the entire previous day
-  #we should make the period and interval variables
-  symbolHistory = symbol.history(period="1d",interval="1m")
+def stockInfo(symbol, period, interval):
+    # this initializes this shit or sum
+    symbol = yf.Ticker(symbol)
 
-  #Each item is just stored in a list within a list
-  #this takes the first value of all the Closed values
-  #could potentially iterate through it
-  #if you just do print(symbolHistory) then it will give you everything
-  print(symbolHistory)
+    # gets all the information each minute of the entire previous day
+    # we should make the period and interval variables
+    symbolHistory = symbol.history(period=period, interval=interval)
 
-  print(symbolHistory["Close"][1])
-  
+    # Each item is just stored in a list within a list
+    # this takes the first value of all the Closed values: symbolHistory["Close"][1]
+    # could potentially iterate through it
+    # if you just do print(symbolHistory) then it will give you everything
 
-  #bruh 
-  return symbolHistory["Close"][-1]
 
-  
-stockInfo("SPY")
+
+    # bruh
+    return symbolHistory["Close"][-1]
+
+
+#print(stockInfo("AAPL", "1wk", "1d"))
--- main.py
+++ main.py
@@ -4,15 +4,40 @@ import api_functions
 app = Flask(__name__)
 
 
-def fetchStockInfo(symbol):
-    return str(api_functions.stockInfo(symbol))
+def fetchStockInfo(symbol, period, interval):
+    return str(api_functions.stockInfo(symbol, period, interval))
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         symbol = request.form.get("symbol", "")
 
-        return render_template("displayStock.html", info=fetchStockInfo(symbol))
+        #default period/interval
+        period = "1d"
+        interval = "1m"
+
+        if request.form.get('hour') == "1h":
+            period = "1h"
+            interval = "1m"
+        elif request.form.get("day") == "1d":
+            period = "1d"
+            interval = "1m"
+        elif request.form.get("week") == "1wk":
+            period = "1wk"
+            interval = "1d"
+        elif request.form.get("month") == "1mo":
+            period = "1mo"
+            interval = "1d"
+        elif request.form.get("sixmonths") == "6mo":
+            period = "6mo"
+            interval = "1d"
+        elif request.form.get("year") == "1y":
+            period = "1y"
+            interval = "1wk"
+            
+            
+            
+        return render_template("displayStock.html", info=fetchStockInfo(symbol, period, interval))
 
     return render_template("index.html")
 
